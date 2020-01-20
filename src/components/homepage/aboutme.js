17ee8747849c4fbaa2f2946a7d39b599ad84e749
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-20 21:42:46 +0600

    Added github, linkedin and stackoverflow to my profile section

--- src/components/homepage/aboutme.js
+++ src/components/homepage/aboutme.js
@@ -5,7 +5,6 @@ import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
 import styles from "../styles/aboutmeStyles"
-
 function AboutMeDrawer(props) {
   const { classes } = props
 
@@ -38,6 +37,52 @@ function AboutMeDrawer(props) {
               prottoyrudra7777@gmail.com
             </a>
           </Typography>
+
+          <div className={classes.linkContainer}>
+            <Typography
+              variant="subtitle2"
+              component="span"
+              className={classes.github}
+            >
+              <a
+                href="https://github.com/Prottoy2938"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Github
+              </a>
+            </Typography>
+
+            <Typography
+              variant="subtitle2"
+              component="span"
+              className={classes.stackoverflow}
+            >
+              <a
+                href="https://stackoverflow.com/users/12200634/prottay-rudra"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Stackoverflow
+              </a>
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              component="span"
+              className={classes.linkedin}
+            >
+              <a
+                href="https://www.linkedin.com/in/prottoy-rudra-4b941b177/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Linkedin
+              </a>
+            </Typography>
+          </div>
         </CardContent>
       </Card>
     </div>

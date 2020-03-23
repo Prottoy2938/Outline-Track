eeeddea945dd2970e1f627b635c6df72f6a2b0f1
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-03-23 21:36:57 +0600

    changed file name convention from camelCase to regular -and modified propTypes on themecontext.js

--- src/components/common/theme.context.js
+++ src/components/common/theme.context.js
@@ -1,12 +1,12 @@
 import React, { createContext, useState, useEffect } from "react"
-
+import PropTypes from "prop-types"
 export const ThemeContext = createContext()
 export function ThemeProvider(props) {
   const [isDarkMode, setDarkMode] = useState(true)
-
   useEffect(() => {
     setDarkMode(JSON.parse(localStorage.getItem("darkMode")))
   }, [])
+
   const toggleDarkMode = () => {
     localStorage.setItem("darkMode", !isDarkMode)
     setDarkMode(!isDarkMode)
@@ -22,4 +22,8 @@ export function ThemeProvider(props) {
       {props.children}
     </ThemeContext.Provider>
   )
+}
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node,
 }

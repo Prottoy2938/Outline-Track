6b5a8647f04fabecb7594b57a8441a76ba642a32
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-06-13 16:59:16 +0600

    Upaded image-search-mobileNet page to redirect to a new url - changed url to https://cluster-11.github.io/image-search-mobileNet/

---
+++ src/pages/webapps/image-search-mobileNet.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+export default () => {
+  //This web applications url has changed, those who has the old url which brings them here will be redirected
+  //to a new url
+  useEffect(() => {
+    window.location.replace(
+      "https://cluster-11.github.io/image-search-mobileNet/"
+    )
+  }, [])
+  return null
+}
